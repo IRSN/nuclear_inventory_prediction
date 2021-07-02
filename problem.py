ed2434abf7ea35d6057944c657a9f6f082efd553
Francois Caud <frcaud@gmail.com>
2021-07-02 16:05:00 +0200

    fix problem.py

--- problem.py
+++ problem.py
@@ -6,7 +6,7 @@ from sklearn.utils import shuffle
 from rampwf.score_types.base import BaseScoreType
 from sklearn.model_selection import ShuffleSplit
 
-problem_title = "Nuclear inventory of a nuclear reactor core in operation"
+problem_title = "Isotopic inventory of a nuclear reactor core in operation"
 
 _target_names = ["Y_" + j for j in list(string.ascii_uppercase)]
 
@@ -24,7 +24,7 @@ class MSE(BaseScoreType):
         self.precision = precision
 
     def __call__(self, y_true, y_pred):
-        mse = (np.square(y_true - y_pred).sum(axis=1)).mean()
+        mse = (np.square(y_true - y_pred)).mean()
         return mse
 
 
@@ -38,7 +38,7 @@ class MAPE(BaseScoreType):
         self.precision = precision
 
     def __call__(self, y_true, y_pred):
-        mape = (np.fabs(y_true - y_pred) / y_true).mean().sum()
+        mape = (np.abs(y_true - y_pred) / y_true).mean()
         return mape
 
 
@@ -51,7 +51,7 @@ score_types = [
 def _get_data(path=".", split="train"):
     # load pre-prepared dataset aggregating all of the different input data
     # ( for the training dataset, these are composed of 920 different
-    # simulation of an operating reactor )
+    # simulations of an operating reactor )
     dataset = pickle.load(
         open(os.path.join(path, "data", f"{split}_data_python3.pickle"), "rb")
     )
@@ -59,17 +59,17 @@ def _get_data(path=".", split="train"):
     # Isotopes are named from A to Z
     alphabet = list(string.ascii_uppercase)
 
-    # At T=0 only isotopes from A to H are != 0. Those are the input parameters
-    # The input parameter space is composed of those initial compositions
+    # At T=0 only isotopes from A to H are != 0. Those are the input composition
+    # The input parameter space is composed of those initial compositions + operating parameters p1 to p5
     input_params = alphabet[:8] + ["p1", "p2", "p3", "p4", "p5"]
 
-    data = dataset_norm[alphabet].add_prefix("Y_")
-    data["times"] = dataset_norm["times"]
+    data = dataset[alphabet].add_prefix("Y_")
+    data["times"] = dataset["times"]
     data = data[data["times"] > 0.0]
 
     temp = pd.DataFrame(
-        np.repeat(dataset_norm.loc[0][input_params].values, 80, axis=0),
-        columns=input_params,
+        np.repeat(dataset.loc[0][input_params].values, 80, axis=0),
+        columns=input_params
     ).reset_index(drop=True)
     data = pd.concat([temp, data.reset_index(drop=True)], axis=1)
 
